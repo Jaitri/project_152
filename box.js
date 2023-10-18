a9fd16dba84b57627429fe20bbd99717e94bad83
Jaitri <126093492+Jaitri@users.noreply.github.com>
2023-10-18 22:02:07 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent("move-box",{
+    schema:{
+        moveY:{ type:"number", default:1},
+        rotY:{type:"number", default:45}
+    },
+    tick: function(){
+        window.addEventListener("click", e=> {
+            this.data.clickCounter = this.data.clickCounter + 1;
+        });
+            if (this.data.clickCounter === 1){
+              const rotation = { x: 0, y: 20, z:0};
+              this.el.setAttribute("rotation", rotation);
+
+             } else if(this.data.clickCounter === 2)
+            {
+                const rotation = {x:0, y:100, z:0};
+            }
+        }
+};
